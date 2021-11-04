afa650f84c8d13123e43f4df7eff92990b94cab6
AsiaSzych <83085243+AsiaSzych@users.noreply.github.com>
2021-11-04 21:50:30 +0100

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -1,17 +1,7 @@
 from os import getcwd
 import pandas as pd
-import io
-import requests
-import pathlib
 
-destination = getcwd()
-filename = destination + "/heart.csv"
-file = pathlib.Path(filename)
-if not file.exists():
-    url = "https://raw.githubusercontent.com/ANRDP/ANRDP/main/heart.csv"
-    data = requests.get(url)
-    dataframe = pd.read_csv(io.StringIO(data.content.decode('utf-8')))
-    dataframe.to_csv(filename)
-else:
-    dataframe = pd.read_csv(filename)
-    print(dataframe.head)
+directory = getcwd()
+filename = directory + "/heart.csv"
+dataframe = pd.read_csv(filename)
+print(dataframe.head)
