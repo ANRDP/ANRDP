7d61d808fd87c680737d17d437b4e83e9ac3bd1e
AsiaSzych <83085243+AsiaSzych@users.noreply.github.com>
2021-12-13 21:44:25 +0100

    Update DesicionTreeAlgorithm.py

--- DesicionTreeAlgorithm.py
+++ DesicionTreeAlgorithm.py
@@ -6,6 +6,8 @@ from numpy import asarray
 from sklearn import metrics, preprocessing, tree
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.tree import export_graphviz 
+import pydotplus
 
 directory = getcwd()
 filename = directory + "/heart.csv"
@@ -25,7 +27,7 @@ target = dataframe["HeartDisease"]
 
 features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.3, random_state=1)
 
-clf = DecisionTreeClassifier(max_depth=3)
+clf = DecisionTreeClassifier(max_depth=5)
 
 clf = clf.fit(features_train, target_train)
 
@@ -33,13 +35,19 @@ target_predict = clf.predict(features_test)
 
 print("Accuracy: ", metrics.accuracy_score(target_test, target_predict))
 
-fig = plt.figure(figsize=(20,20))
-_ = tree.plot_tree(clf,
-                   feature_names=features.columns,
-                   filled=True)
-fig.savefig("decistion_tree.png")
+# fig = plt.figure(figsize=(20,20))
+# _ = tree.plot_tree(clf,
+#                    feature_names=features.columns,
+#                    filled=True)
+# fig.savefig("decistion_tree.png")
 
-#Cross Validation
+dot_data = export_graphviz(clf, filled=True, rounded=True,
+                                    feature_names=features.columns,
+                                    out_file=None)
+graph = pydotplus.graph_from_dot_data(dot_data)
+graph.write_png(directory+'/tree.png')
+
+#Cross Validations
 number=10
 scores = cross_val_score(clf, features, target, cv=number, scoring="f1")
 
@@ -48,5 +56,30 @@ for i in scores:
     sum += i
 
 average = sum/number
+print(f"F1 score after cross validation: {average}")
 
+max_depth = []
+fscore = []
+for i in range(1,30):
+    dtree = DecisionTreeClassifier(max_depth=i)
+    dtree.fit(features_train, target_train)
+    pred = dtree.predict(features_test)
+    number=10
+    scores = cross_val_score(clf, features, target, cv=number, scoring="f1")
+    sum=0
+    for j in scores:
+        sum += j
+    average = sum/number
+    # acc_gini.append(metrics.f1_score(target_test, pred, average='binary'))
+    fscore.append(average)
+    max_depth.append(i)
+    d = pd.DataFrame({'F1 score':pd.Series(fscore), 
+    'max_depth':pd.Series(max_depth)})
+# visualizing changes in parameters
+
+plt.plot('max_depth','F1 score', data=d, label='F1 score')
+plt.xlabel('max_depth')
+plt.ylabel('F1 score')
+plt.legend()
+plt.show()
 
