259e6f413e366778bca240c65f7d3b94542efe9f
Dominika Piotrowska <83085274+dopiotrowska@users.noreply.github.com>
2022-01-02 21:41:58 +0100

    code refactoring

--- adaboost.py
+++ adaboost.py
@@ -1,15 +1,9 @@
 from os import getcwd
 import pandas as pd
-from matplotlib import pyplot as plt
-import numpy as np
-from numpy import asarray
-from sklearn import metrics, preprocessing, tree
+from sklearn import metrics
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split, cross_validate
-from sklearn.tree import export_graphviz 
-import pydotplus
 from yellowbrick.classifier import ClassPredictionError
-
 
 directory = getcwd()
 filename = directory + "/heart.csv"
@@ -21,41 +15,34 @@ categorical = dataframe[["Sex", "ChestPainType", "RestingECG", "ExerciseAngina",
 categorical = dataframe.select_dtypes(include=object).columns
 dataframe = pd.get_dummies(dataframe, columns=categorical, drop_first=True)
 
-
 features = dataframe.drop("HeartDisease", axis=1)
 target = dataframe["HeartDisease"]
 
-features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.1, random_state=1)
-#X_train, X_test, y_train, y_test 
+features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.1,
+                                                                            random_state=1)
 
-
-clf = AdaBoostClassifier(n_estimators=100)
-
+# AdaBoost
+clf = AdaBoostClassifier(n_estimators=100, learning_rate=0.6)
 clf = clf.fit(features_train, target_train)
-
-target_predict = clf.predict(features_test) #y_pred
-
+target_predict = clf.predict(features_test)
 target_train_pred = clf.predict(features_train)
-
 ab_f1 = metrics.f1_score(target_test, target_predict)
 
-
+# F-score without cross validation
 print(f"F-SCORE: {ab_f1}")
 
+# Visualize Class Predicition Error for AdaBoost Classifier
 visualizer = ClassPredictionError(clf)
-
-# Evaluate the model on the test data
+# Fit training data
 visualizer.fit(features_train, target_train)
-
+# Evaluate the model on test data
 visualizer.score(features_test, target_test)
+visualizer.show()
 
-visualizer.poof()
+# Cross Validation
+ab_cv = AdaBoostClassifier(n_estimators=100, learning_rate=0.6)
+ab_cv_scores = cross_validate(ab_cv, features_train, target_train, scoring=["accuracy", "precision", "recall", "f1"],
+                              cv=10)
+ab_cv_scores = pd.DataFrame(ab_cv_scores, index=range(1, 11))
 
-
-#Cross Validation
-rf_xvalid_model = AdaBoostClassifier(n_estimators=100)
-
-rf_xvalid_model_scores = cross_validate(rf_xvalid_model, features_train, target_train, scoring = ["accuracy", "precision", "recall", "f1"], cv = 10)
-rf_xvalid_model_scores = pd.DataFrame(rf_xvalid_model_scores, index = range(1, 11))
-
-print(f"After cross validation: \n {rf_xvalid_model_scores.mean()[2:]}")
+print(f"Scores after cross validation:\n{ab_cv_scores.mean()[2:]}")
